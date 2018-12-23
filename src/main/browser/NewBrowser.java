3824565ab172bf8c7bbfe737a74c7389c2f86ef0
Brandon <Brandon@Laptop15-bs0xx>
2018-12-22 21:06:27 -0500

    Working on a tab panel with resizable frame.

--- src/main/browser/NewBrowser.java
+++ src/main/browser/NewBrowser.java
@@ -10,28 +10,35 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author Brandon
  */
 public class NewBrowser extends Application {
-   
+    private double xOffset = 0;
+    private double yOffset = 0;
+    
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws Exception {    
         Parent root = FXMLLoader.load(getClass().getResource("webfxml.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/main/resources/css/main.css");
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment()
         .getDefaultScreenDevice();
+        //gets resolution info
         int width = gd.getDisplayMode().getWidth();
         int height = gd.getDisplayMode().getHeight();
-        //gets resolution info
+        
         System.out.println("Resolution: " + height + ", " + width);
         if(width < 720){
-        Image fireIcon = new Image("/main/resources/icons/firebox_logo_32px.png");
+         Image fireIcon = new Image("/main/resources/icons/firebox_logo_32px.png");
         stage.getIcons().add(fireIcon);
         }
         else if(width >= 720|| width < 1000 ){
@@ -56,12 +63,35 @@ public class NewBrowser extends Application {
         }
         stage.setMinHeight(height/2.5);
         stage.setMinWidth(width/3);
+        stage.initStyle(StageStyle.UNDECORATED);
         
-        stage.setTitle("Hotbox");
+        //moveable scene
+        root.setOnMousePressed((MouseEvent event) -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged((MouseEvent event) -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+        BorderPane borderPane = new BorderPane();
+        borderPane.setStyle("-fx-background-color: white;");
+        ToolBar toolBar = new ToolBar();
+
+        int tHeight = 25;
+        toolBar.setPrefHeight(tHeight);
+        toolBar.setMinHeight(tHeight);
+        toolBar.setMaxHeight(tHeight);
+
+        borderPane.setTop(toolBar);
+        stage.setTitle("Fireybox");
         stage.setScene(scene);
         stage.show();
         stage.setY((int)(height/7.87)-((int)height/(4.5533*4.5533)));
-        stage.setX((int)(width/4.5533));
+        stage.setX((int)(width/4.5533));     
+        WindowStyle.allowDrag(root, stage);
+        WindowStyle.stageDimension(stage.getWidth(), stage.getHeight());
+        System.out.println("MaxWidth " + width + " X MaxHeight " + height);
         
     }
     
@@ -70,8 +100,7 @@ public class NewBrowser extends Application {
      */
     public static void main(String[] args) {
         System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
-        launch(args);
-        
+        launch(args); 
     }
 
 }
